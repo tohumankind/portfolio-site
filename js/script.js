ada85bfaa7a75ee6e79b6ffa8bfcf9ea4d7def78
sam taylor <taylorsd3@vcu.edu>
2020-01-08 16:06:10 -0500

    navigation update

--- js/script.js
+++ js/script.js
@@ -50,3 +50,23 @@ ELEMENTS.forEach((element, index) => {
 		ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
 	});
 });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
