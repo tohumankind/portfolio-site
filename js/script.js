848f168486181ddb3a82a2b9a969046b39ae6de5
sam taylor <taylorsd3@vcu.edu>
2020-03-12 16:45:21 -0400

    Updated Projects

--- js/script.js
+++ js/script.js
@@ -72,6 +72,8 @@ window.onclick = function(event) {
 
 dragElement(document.getElementById("window"));
 dragElement(document.getElementById("window2"));
+dragElement(document.getElementById("window3"));
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
